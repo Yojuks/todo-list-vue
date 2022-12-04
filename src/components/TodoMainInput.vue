3fbf06f24e94a5c93e90450a1e962991d0984e31
Andrii_Yozh <yojuks@gmail.com>
2022-12-04 15:13:19 +0200

    feat: create new component

---
+++ src/components/TodoMainInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <NewTask />
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import NewTask from '../components/NewTask.vue';
+import TodoList from '../components/TodoList.vue';
+
+export default {
+  name: 'TodoMainInput',
+  props: {
+    msg: String
+  },
+
+  components: {
+    NewTask,
+    TodoList
+  },
+
+  data() {
+    return {
+      inputText: '',
+      tasks: []
+    }
+  },
+
+  mounted() {
+    try {
+      this.$store.dispatch('fetchTasks')
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'addNewTask',
+      'removeTask',
+    ]),
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
