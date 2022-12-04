72e488cf65830687161ca96b0f29c23e88fbfbf9
Andrii_Yozh <yojuks@gmail.com>
2022-12-04 15:07:57 +0200

    feat: add imports and setting into new Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify: new Vuetify(),
+  store,
+  render: (h) => h(App)
+}).$mount("#app");
