fa2502401ec7e1e0f0d8d2ca08c04f52a6694add
Andrii_Yozh <yojuks@gmail.com>
2022-12-04 15:10:28 +0200

    feat: create new component TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <ul>
+            <TodoListElement v-for="task in tasks" :key="task.id" :task="task" />
+        </ul>
+    </div>
+</template>
+  
+<script>
+import { mapActions } from 'vuex';
+import TodoListElement from '../components/TodoListElement.vue';
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'TodoList',
+    computed: {
+        ...mapGetters({
+            tasks: 'getAllTasks'
+        })
+    },
+
+    methods: {
+        ...mapActions([
+            'removeTask',
+        ]),
+    },
+
+    components: {
+        TodoListElement,
+    },
+}
+</script>
+  
+<style scoped >
+ul {
+    padding: 0;
+}
+</style>
+  
